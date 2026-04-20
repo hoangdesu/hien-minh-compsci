0a37df97d90382023a0ca47e1d31f99945440cf2
hoangdesu <hoangdesu@gmail.com>
2026-04-20 20:10:11 +0700

    loops and list

---
+++ apr18-loops.py
@@ -0,0 +1,44 @@
+# Enter start: 3
+# Enter stop: 7
+
+# 3 4 5 6 7
+
+# start = int(input('Enter start: '))
+# stop = int(input('Enter stop: '))
+
+# # mặc định là chiều đi lên
+# step = 1
+
+# # đi xuống
+# if start > stop:
+#     step = -1
+    
+# for i in range(start, stop + step, step):
+#     print(i, end=" ")
+
+# print()
+
+
+# Remember the values user inputs
+
+# Enter sport 1: basketball
+# Enter sport 2: bouldering
+# Enter sport 3: running
+
+
+# I love basketball
+# I love bouldering
+# I love running
+
+
+sports = []
+
+print(f'{sports=}')
+
+# Add items into a list
+sports.append('cafe')
+sports.append('sua da')
+
+print(f'{sports=}')
+
+
