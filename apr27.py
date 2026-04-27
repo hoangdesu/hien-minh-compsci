7dc236a681de73d29beeb7c5d61039be62cb8612
hoangdesu <hoangdesu@gmail.com>
2026-04-28 00:11:45 +0800

    list and loops

---
+++ apr27.py
@@ -0,0 +1,80 @@
+# Variables: biến
+
+# my_name = 'Minh'
+# print(my_name)
+
+# my_name = 'Hiển Minh'
+# print(my_name)
+
+
+# num_a = 1
+
+# num_a = 3
+
+# num_a = num_a + 2
+
+# num_a = 1
+# num_a += 10 
+# # the same as: num_a = num_a + 10
+
+# print(num_a)
+
+
+# Calculator app using while loop:
+
+# total = 0
+# num_list = []
+
+# num = int(input('Enter a number: '))
+
+# while num != 0:
+#     total += num
+#     num_list.append(num)
+#     num = int(input('Enter a number: '))
+
+# # print(f'Total = {total}')
+
+# for i in range(len(num_list)):
+#     print(num_list[i], end="")
+    
+#     # giá trị cuối cùng, thêm dấu = 
+#     if i == len(num_list) - 1:
+#         print(' = ', end="")
+#     else:
+#         print(' + ', end="")
+        
+# print(total)
+
+
+
+Homework: 
+    
+User có thể nhập vào 3 input:
+    
+Even or odd numbers: even (options: 'even' | 'odd')
+Starting point: 8
+Total even numbers: 5
+
+8 10 12 14 16
+
+---
+Even or odd numbers: odd
+Starting point: 11
+Total even numbers: 8
+
+11 13 15 17 19 21 23 25
+
+
+---
+Even or odd numbers: even
+Starting point: 11
+Total even numbers: 5
+
+12 14 16 18 20
+
+---
+Even or odd numbers: odd
+Starting point: 10
+Total even numbers: 5
+
+11 13 15 17 19
