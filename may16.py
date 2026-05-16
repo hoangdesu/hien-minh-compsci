b575d8abf499483e5e060aca74ac306a73e2b433
hoangdesu <hoangdesu@gmail.com>
2026-05-16 12:04:09 +0800

    linear search, big O

---
+++ may16.py
@@ -0,0 +1,33 @@
+# Linear search
+
+# # menu = ['pho', 'com tam', 'hu tiu', 'bun rieu', 'bun cha', 'xoi']
+
+# # print(menu)
+
+# order = input('What do you want to order: ')
+
+# nếu order có trong menu:
+#     print "com tam is coming"
+# nếu order KO có trong menu:
+#     print "sorry we don't have sushi"
+
+
+import random
+numbers = [random.randint(1, 20) for _ in range(10)]
+
+print(numbers)
+
+# search number: 17
+# search number 17 is at index 2
+
+# search number: 12
+# search number 12 is at index 4
+
+# search number: 3
+# search number 3 is not in the list
+
+[11, 1, 17, 4, 12, 4, 12, 11, 2, 9]
+
+search for numbers larger than: 9
+11, 17, 12, 12, 11
+
